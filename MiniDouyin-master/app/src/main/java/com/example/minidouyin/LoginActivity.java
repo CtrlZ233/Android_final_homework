7e828e8f2c84e10679ad6a409fbca27111fa7374
CtrlZ <328825619@qq.com>
2019-07-22 14:34:07 +0800

    大作业

---
+++ MiniDouyin-master/app/src/main/java/com/example/minidouyin/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.minidouyin;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SharedPreferences lock = getSharedPreferences("lock",MODE_PRIVATE);
+        SharedPreferences.Editor editor = getSharedPreferences("lock",MODE_PRIVATE).edit();
+        String judge = lock.getString("user_name","");
+        if(judge.equals("")){
+            startActivity(new Intent(LoginActivity.this, UserInfoActivity.class));
+        }
+        else  startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        finish();
+    }
+}
+
